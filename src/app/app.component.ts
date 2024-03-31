9945fb7a664b9b6dea756fe755000af59a27f4d3
AntonisTaramigkos <124712913+AntonisTaramigkos@users.noreply.github.com>
2024-03-31 23:39:13 +0300

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-import { PersonTableComponent } from './compoments/person-table/person-table.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 import { Person } from './shared/interfaces/person';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent, EventBindExampleComponent],
+  imports: [RouterLink, RouterOutlet,  PersonTableComponent, EventBindExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {  
-  name = "Antonis";
+export class AppComponent {   
 
   person0 = {
       givenName : "Antonis",
