52d059f53c5d0e25ad247f5571c61a435008add4
AntonisTaramigkos <124712913+AntonisTaramigkos@users.noreply.github.com>
2024-04-02 19:01:44 +0300

    sorting data

--- src/app/components/simple-datatable-example/simple-datatable-example.component.ts
+++ src/app/components/simple-datatable-example/simple-datatable-example.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { ManyPerson } from 'src/app/shared/interfaces/person';
+import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
 
 @Component({
   selector: 'app-simple-datatable-example',
   standalone: true,
-  imports: [],
+  imports: [SimpleDatatableComponent],
   templateUrl: './simple-datatable-example.component.html',
   styleUrl: './simple-datatable-example.component.css'
 })
 export class SimpleDatatableExampleComponent {
-  // @Input() data:EPerson[];
-  manyperson = ManyPerson;
-
-
+manyperson = ManyPerson;
 }
--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { sortBy } from 'lodash-es';
 import { EPerson } from 'src/app/shared/interfaces/person';
 
 @Component({
@@ -10,5 +11,40 @@ import { EPerson } from 'src/app/shared/interfaces/person';
 })
 export class SimpleDatatableComponent {
   @Input() data:EPerson[];
+  
+  sortOrder = {
+    givenName:"none",
+    surName : "none",
+    age:"none",
+    email:"none",
+    education: "none"
+  }
+// sortkey = givenname/surname ktlp
+  sortData(sortKey:string){
+    if(this.sortOrder[sortKey]==="asc"){
+      this.sortOrder[sortKey] = "desc";
+      this.data = sortBy(this.data, sortKey).reverse();
+    }else {
+      this.sortOrder[sortKey]="asc";
+      this.data = sortBy(this.data, sortKey)
+    }
 
+    for(let key in this.sortOrder){
+      if(key !== sortKey){
+        this.sortOrder[key] = "none";
+      }
+    };
+    sortSign(sortKey: string) {
+      if (this.sortOrder[sortKey] === 'asc') {
+        return '↑';
+      } else if (this.sortOrder[sortKey] === 'desc') {
+        return '↓';
+      } else {
+        return '';
+      }
+    }
+
+    }
+
+  }
 }
